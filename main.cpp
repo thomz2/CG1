724dbfc5e2f8038f43c4e2251eaf09c24b07b1f1
Thomaz <thomaz2pinheiro@gmail.com>
2023-08-21 21:12:56 -0300

    terminando as coisas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include "classes/headers/vec3.h"
+#include "classes/headers/Vec3.h"
+#include "classes/headers/Canvas.h"
+#include "classes/headers/Esfera.h"
+#include "classes/headers/Ray.h"
 
 using namespace std;
 
@@ -30,6 +33,39 @@ int main ( int argc, char *argv[] ) {
     SDL_Renderer *renderer;
 
     initializeSDLAndWindow(&window, &renderer, wJanela, hJanela);
+
+    const double dJanela = 100;
+    const double rEsfera = 20;
+    const double zCentroEsfera = - (dJanela + rEsfera) - 60; // sempre diminuindo um valor
+
+    Vec3 centroJanela(0, 0, -dJanela);
+    Vec3 olhoPintor(0, 0, 0);
+
+    SDL_Color corEsfera = {255, 0, 0, 255};
+
+    const double nCol = wJanela / 2;
+    const double nLin = hJanela / 2;
+
+    const double Dx = wJanela / nCol;
+    const double Dy = hJanela / nLin;
+
+    for (int l = 0; l < nLin; ++l) {
+        
+        double y = hJanela/2 - Dy/2 - l*Dy;
+        
+        for (int c = 0; c < nCol; ++c) {
+
+            double x = -wJanela/2 + Dx/2 + c*Dx;
+
+            Vec3 PosJanela(x, y, -dJanela);
+             
+        }
+    }
+
+    // usar no loop
+    // const double x = - wJanela/2 + Dx/2  + c*Dx;
+    // const double y =   hJanela/2 - Dy/2  - l*Dy;
+
 
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 1);
     SDL_RenderDrawPoint(renderer, 45, 59);
